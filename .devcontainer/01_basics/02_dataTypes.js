d2d9754bf8d0dc5cc7bdb13331145bde728f4a66
Ankit Kumar Mishra <98024884+ankitlpu@users.noreply.github.com>
2024-02-20 16:16:12 +0000

    DataTypes

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,12 @@
+// number => 2 to power 52
+//bigint
+//boolean => true/false
+//null => standalone value
+//undefined => not any data type
+//symbol => unique
+
+
+// object 
+
+console.log(typeof undefined);
+console.log(typeof null);
