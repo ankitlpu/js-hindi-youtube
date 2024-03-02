b70735c97646ca90221a6f92f77fdb0463a67314
Ankit Kumar Mishra <98024884+ankitlpu@users.noreply.github.com>
2024-03-02 06:15:26 +0000

    Conversion

---
+++ .devcontainer/01_basics/03_conversionOperator.js
@@ -0,0 +1,16 @@
+let isloggedIn = "Ankit"
+
+let booleanLogged = Boolean(isloggedIn);
+
+// console.log(booleanLogged);
+
+// 1 => true; 0 => false;
+// "" => false;
+//"Ankit" => true;
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+
+console.log(typeof stringNumber);
